7515af6673c8f0ba2c280994c757b67c36f99161
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-02-16 12:29:02 +0530

    HomePage change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,30 +4,37 @@ import styles from "./home.module.css"
 const Home = () => {
   return (
    <div className={styles.container}>
-    <div className={styles.sideBar}>
-      <div className={styles.panel}>List of Restaurents
-        {/* <Image src="/icon.png" alt="" fill className={styles.panelImg}/> */}
-      </div>
-        {/* <div className={styles.panelText}>
+     <div className={styles.item1}>
+       <div className={styles.t1}>List of Restaurants</div>
+       <div className={styles.lBar}>
+       <span>regio de Montreal</span>
+       <li>Laval </li>
+       <li>Monteregie Ouest </li>
+       <li>Rive Nord</li>
+       <li>Lanaudiere</li>
+       <li>Centre de Montreal</li>
+       <li>Est de Montreal </li>
+       <li>Ouest  de Montreal</li>
+       <li>Monteregie Nord</li>
+       <li>Rive Sud</li>
+       <li>Autres regions du Quebec</li>
+     </div>
+     </div>
+     <div className={styles.item2}>
+      <div className={styles.midText}> Grande région de Montréal
 
-        <ul>sfdhgoi</ul>
-        <ul>sfdhgoi</ul>
-        <ul>sfdhgoi</ul>
-        <ul>sfdhgoi</ul>
-        <ul>sfdhgoi</ul>
-        <ul>sfdhgoi</ul>
-        <ul>sfdhgoi</ul>
-        </div> */}
-       
-     
+      <Image src="/img.png" alt="" fill className={styles.midImg}/>
+     </div>
     </div>
-    <div className={styles.midBar}>
-    <Image src="/img.png" alt="" fill className={styles.midImg} />
-    </div>
-    <div className={styles.rightBar}>
-      <Image src="/r_img.png" alt="" fill  className={styles.rImg}>
-      </Image>
-    </div>
+     <div className={styles.item3}>
+      <Image src="/r_img.png" alt="" fill className={styles.rImg}/>
+     </div>
+     <div className={styles.item4}></div>
+     <div className={styles.item5}></div>
+     <div className={styles.item6}></div>
+     <div className={styles.item7}></div>
+     <div className={styles.item8}></div>
+     <div className={styles.item9}></div>
    </div>
    
    )
